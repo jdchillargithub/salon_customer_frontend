3614665f74e7a8cc6e7b1d860cfc116ce808842b
jdchillargithub <134476017+jdchillargithub@users.noreply.github.com>
2024-09-02 16:44:18 +0530

    Delete src directory

--- src/app/pages/doctors-list-new/doctors-list-new.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from "@angular/core";
-
-@Component({
-  selector: "app-doctors-list-new",
-  templateUrl: "./doctors-list-new.component.html",
-  styleUrls: ["./doctors-list-new.component.css"],
-})
-export class DoctorsListNewComponent {}
--- src/environments/environment.prod.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: false,
-  razorpayKey: "rzp_test_eEq43QGYfjTlfD",
-  };
